5a97ab3f851b5e36f6c1d88b495841f1c8998fb1
Patricia <patricianjuguna084@gmail.com>
2025-07-19 10:41:55 +0300

    Lab Complete

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,57 @@
+import Home from "./pages/Home";
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import Movie from "./pages/Movie";
+import ErrorPage from "./pages/ErrorPage";
+import NavBar from "./components/NavBar";
 
+// ✅ Actual array of route objects (what the tests expect)
+export const routes = [
+  {
+    path: "/",
+    element: (
+      <>
+        <NavBar />
+        <Home />
+      </>
+    ),
+    errorElement: (
+      <>
+        <NavBar />
+        <ErrorPage />
+      </>
+    ),
+  },
+  {
+    path: "/directors",
+    element: (
+      <>
+        <NavBar />
+        <Directors />
+      </>
+    ),
+  },
+  {
+    path: "/actors",
+    element: (
+      <>
+        <NavBar />
+        <Actors />
+      </>
+    ),
+  },
+  {
+    path: "/movie/:id",
+    element: (
+      <>
+        <NavBar />
+        <Movie />
+      </>
+    ),
+  },
+];
 
-const routes = [
-   
-  ];
-
-export default routes;
+// ✅ For the actual app, we still export the router
+import { createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter(routes);
+export default router;
